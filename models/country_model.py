65c0a3c3956ccf6470dd99c16e8a9e16a944ed71
StefanBeleuz <stefan.beleuz@yahoo.com>
2020-12-08 18:07:00 +0200

    Added doc for orm model and repo.

--- models/country_model.py
+++ models/country_model.py
@@ -1,3 +1,8 @@
+"""This module is responsible for modeling a Country from a SQL object to a Python object.
+
+It uses SQLAlchemy as on ORM.
+"""
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, Float, String
 
@@ -5,20 +10,54 @@ Base = declarative_base()
 
 
 class Country(Base):
+    """
+    A class used to represent a Country, modeled from database.
+
+    Attributes
+    ----------
+    id : int
+        un unique id having the role of primary key in database
+    name : str
+        the name of the country
+    capital: str
+        the capital of the country
+    population: int
+        the population of the country
+    density: float
+        the density of the country
+    area: float
+        the area of the country
+    neighbours: str
+        the neighbours of the country, separated by comma
+    language: str
+        the official languages of the country, separated by comma if there are more
+    time_zone: str
+        the time zone of the country
+    government: str
+        the government of the country
+
+    Methods
+    -------
+    to_json()
+        Returns the Country's attributes stored in a dictionary.
+    """
+
     __tablename__ = 'countries'
 
-    id = Column('id', Integer, primary_key=True)
-    name = Column('name', String)
-    capital = Column('capital', String)
-    population = Column('population', Integer)
-    density = Column('density', Float)
-    area = Column('area', Float)
-    neighbours = Column('neighbours', String)
-    language = Column('language', String)
-    time_zone = Column('time_zone', String)
-    government = Column('government', String)
+    def __init__(self):
+        self.id = Column('id', Integer, primary_key=True)
+        self.name = Column('name', String)
+        self.capital = Column('capital', String)
+        self.population = Column('population', Integer)
+        self.density = Column('density', Float)
+        self.area = Column('area', Float)
+        self.neighbours = Column('neighbours', String)
+        self.language = Column('language', String)
+        self.time_zone = Column('time_zone', String)
+        self.government = Column('government', String)
 
     def to_json(self):
+        """ Returns the Country's attributes stored in a dictionary. """
         return {
             'name': self.name, 'capital': self.capital, 'population': self.population, 'density': self.density,
             'area': self.area, 'neighbours': self.neighbours, 'language': self.language, 'time_zone': self.time_zone,
--- repos/country_repo.py
+++ repos/country_repo.py
@@ -1,3 +1,8 @@
+"""This module is responsible for inserting and getting data from database.
+
+It uses SQLAlchemy as on ORM.
+"""
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models.country_model import Country
@@ -7,10 +12,36 @@ session = sessionmaker(bind=engine)()
 
 
 def get_all_countries():
+    """ Returns all countries stored in the database.
+
+    Returns
+    -------
+    list
+        a list of all countries in the database
+    """
     return session.query(Country).all()
 
 
 def get_top_countries(limit, field, order):
+    """Returns first countries from database sorted by field in a specific order.
+
+    Parameters
+    ----------
+    limit : int
+        The number of countries returned.
+    field: str
+        The attribute of the Country to filter countries by.
+    order: str
+        Specify the order in which the countries are sorted.
+        If order is 'asc' then countries are listed ascending, else countries are listed descending.
+
+    Returns
+    -------
+    list
+        a list of countries based on the passed in parameters.
+    None
+        if an unknown Country attribute is passed in as a parameter.
+    """
     try:
         if order == 'asc':
             return session.query(Country).order_by(getattr(Country, field)).limit(limit).all()
@@ -21,6 +52,22 @@ def get_top_countries(limit, field, order):
 
 
 def get_filtered_countries(dict_filters):
+    """Returns countries from database filtered by a number of filters.
+
+        Parameters
+        ----------
+        dict_filters : dict
+            The filters to be applied.
+            key   = Country attribute
+            value = filter value
+
+        Returns
+        -------
+        list
+            a list of filtered countries based on the passed in parameters.
+        None
+            if an unknown Country attribute is passed.
+        """
     try:
         filters = []
         for key, value in dict_filters.items():
@@ -42,6 +89,14 @@ def get_filtered_countries(dict_filters):
 
 
 def insert_countries(countries):
+    """Insert a list of countries into database.
+
+        Parameters
+        ----------
+        countries : list[Country]
+            The list of countries to be stored into database.
+    """
+
     print('Inserting countries into database...')
     session.add_all(countries)
     session.commit()
